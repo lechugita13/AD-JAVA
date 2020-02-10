739db01fa43fbad9c3d5d1952cfc772f061fff5a
guillem <guillemclar@gmail.com>
2020-02-10 14:01:01 +0100

    robotsSQLiteFALTAACABAR

---
+++ RoboticsSQLite/app/src/main/java/com/example/roboticssqlite/Robot.java
@@ -0,0 +1,21 @@
+package com.example.roboticssqlite;
+
+import java.io.Serializable;
+
+public class Robot implements Serializable {
+
+    public String nombre;
+    public String material;
+    public int anyo;
+    public Tipo tipo;
+
+    public Robot() {
+    }
+
+    public Robot(String nombre, String material, int anyo, Tipo tipo) {
+        this.nombre = nombre;
+        this.material = material;
+        this.anyo = anyo;
+        this.tipo = tipo;
+    }
+}
---
+++ RoboticsSQLite/app/src/main/java/com/example/roboticssqlite/Tipo.java
@@ -0,0 +1,9 @@
+package com.example.roboticssqlite;
+
+import java.io.Serializable;
+
+public enum Tipo implements Serializable {
+    R2D2,
+    BENDER,
+    WALLE
+}
